68e469a12f336d96741927a806aab88bab08fd16
Waqas Latef <wikkii@msn.com>
2014-11-03 14:21:18 -0500

    No Bluetooth Done
    You can fork now.

---
+++ MawsomNoBL/app/src/main/java/com/mawsom/mawsom/mawsomnobl/TempCheck.java
@@ -0,0 +1,25 @@
+package com.mawsom.mawsom.mawsomnobl;
+
+/*
+ * This Activities does the temperature check from the arduino data stream and returns
+ * values to the main activity to control the thermostat
+ * 
+ * 
+ * Also includes the Energy saving/checking algorithm used. 
+ * 
+ * 
+ * File IO with the temperature data to the disk space save it in this format 
+ * Time | temperature
+ * 1-2  | 70
+ * find the average of the temp from every hour and save it to auto temp "self learning"
+ * 
+ * 
+ * When away mode: 
+ * take the min temp and max temp from the user input and based on that use the
+ * same average temp algorithm used for normal methods. 
+ */
+
+
+public class TempCheck {
+
+}
---
+++ MawsomNoBL/app/src/main/java/com/mawsom/mawsom/mawsomnobl/UserSave.java
@@ -0,0 +1,11 @@
+package com.mawsom.mawsom.mawsomnobl;
+
+/*
+This Class utilizes the SQLite library in android
+To save user data before updating the app.
+
+ */
+
+public class UserSave {
+
+}
